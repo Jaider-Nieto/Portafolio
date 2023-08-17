4fdeeb45de1e232eafe06990a1913fe5d6865af0
jaider-nieto <jaiderlolxd@gmail.com>
2023-08-17 18:57:42 -0500

    f

--- src/Components/Start/Start.jsx
+++ src/Components/Start/Start.jsx
@@ -7,7 +7,7 @@ const Start = () => {
       <h2 className={style.name}>Jaider Nieto</h2>
       <h2 className={style.dev}>Desarrollador Backend</h2>
       <div className={style.text}> con conocimientos solidos en Frontend </div>
-      <a className={style.btn} href="https://drive.google.com/file/d/1pi9bvq2C1PsDex5Xfq0BV-8SNIZ6MwcB/view?usp=sharing">
+      <a className={style.btn} href="https://drive.google.com/file/d/1CaW55EN0HieRcbjIqNbK2GhoV_fMmlqI/view?usp=drive_link">
         Descargar CV
       </a>
     </div>
