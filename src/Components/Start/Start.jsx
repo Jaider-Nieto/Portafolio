c01759e70c53e6037f81c5631b5b803ac49370a1
jaider-nieto <jaiderlolxd@gmail.com>
2023-10-02 11:52:23 -0500

    responsivo

--- src/Components/Start/Start.jsx
+++ src/Components/Start/Start.jsx
@@ -4,11 +4,11 @@ const Start = () => {
   return (
     <div className={style.container}>
       <div className={style.contImg}>
-      <img
-        className={style.img}
-        src='https://i.ibb.co/268q6sn/yo-img.webp'
-        alt='avatar'
-      />
+        <img
+          className={style.img}
+          src='https://i.ibb.co/268q6sn/yo-img.webp'
+          alt='avatar'
+        />
       </div>
       <p className={style.hi}>Hola, soy</p>
       <p className={style.name}>Jaider Nieto</p>
@@ -16,7 +16,7 @@ const Start = () => {
       <p className={style.text}> con conocimientos solidos en Frontend </p>
       <a
         className={style.btn}
-        href='https://drive.google.com/file/d/1KyNND9eo_Z52DAu92BKq5Nx8neW5VsYC/view?usp=sharing'
+        href='https://drive.google.com/file/d/1_iIAS8kNdmm7x_rZSwhbI5ZrrsGJn_-4/view'
         target='_blank'
       >
         MI CV
