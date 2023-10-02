32132091e8d092e7eaeb27b53153f5dfccd9844d
jaider-nieto <jaiderlolxd@gmail.com>
2023-10-02 10:17:47 -0500

    responsivo

--- src/Components/Proyects/Proyects.jsx
+++ src/Components/Proyects/Proyects.jsx
@@ -94,62 +94,6 @@ const Proyects = () => {
         </div>
       </div>
 
-      {/* Card Pi Pokemons */}
-      {/* <div className={style.cardContainer}>
-        <div className={style.card}>
-          <h3 className={style.cardTitle}>Pi Pokemons</h3>
-          <div className={style.imgCon}>
-            <ul>
-              <li>
-                <img
-                  className={style.cardImg}
-                  src='https://i.ibb.co/5W0q8DW/Pi-pokemon-1.jpg'
-                />
-              </li>
-              <li>
-                <img
-                  className={style.cardImg}
-                  src='https://i.ibb.co/VMd6vv3/Pi-pokemon-2.png'
-                />
-              </li>
-              <li>
-                <img
-                  className={style.cardImg}
-                  src='https://i.ibb.co/8YX481P/Pi-pokemon-3.png'
-                />
-              </li>
-            </ul>
-          </div>
-          <div className={style.cardDesCon}>
-            <p className={style.cardDescription}>
-              <span className={style.resal}>App de Pokemons que incluye:</span>
-              <br />
-              Filtros en conjunto, formulario para la creación de un Pokemon,
-              paginado, ordenamientos, api rest y crud completo, etc.
-            </p>
-            <ul className={style.tecno}>
-              <li>
-                <span className={style.resal}>Front:</span> React, Redux y CSS.
-              </li>
-              <li>
-                <span className={style.resal}>Back:</span> Node.js, Express.
-              </li>
-              <li>
-                <span className={style.resal}>DataBase:</span> PostgreSQL y
-                Sequelize.
-              </li>
-            </ul>
-          </div>
-          <a
-            className={style.btn}
-            target='_blanck'
-            href='https://github.com/Jaider-Nieto/Pi-Pokemon'
-          >
-            Repositorio
-          </a>
-        </div>
-      </div> */}
-
       {/* Card Soul Music*/}
 
       <div className={style.cardContainer}>
